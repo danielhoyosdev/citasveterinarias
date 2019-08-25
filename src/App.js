d9f8069ce62dcdeb31a59a68ac81642badde46fe
Daniel Hoyos <danielhoyos350@gmail.com>
2019-08-25 15:58:40 -0500

    Creación de funcionalidad para registrar cita, validación del formulario y mostrar mensaje de error

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import React from "react";
 import Header from "./components/Header";
 import AgregarCita from "./components/AgregarCita";
 
-function App() {
-  return (
-    <div className="container">
-      	<Header titulo="Citas Veterinarias" />
-		<div className="row">
-				<div className="col-md-6">
-					<AgregarCita />
+class App extends React.Component {
+	state = {
+		citas : []
+	}
+	
+	crearCita = (cita) => {
+		const citas =  [...this.state.citas, cita];
+		this.setState({ citas });
+	}
+
+	render(){
+		return(
+			<div className="container">
+      			<Header titulo="Citas Veterinarias" />
+				<div className="row">
+						<div className="col-md-6">
+							<AgregarCita crearCita={ this.crearCita }/>
+						</div>
+						<div className="col-md-6"></div>
 				</div>
-				<div className="col-md-6"></div>
-		</div>
-    </div>
-  );
+    		</div>
+		);
+	}
 }
 
 export default App;
--- src/components/AgregarCita.js
+++ src/components/AgregarCita.js
@@ -1,52 +1,86 @@
 import React from "react";
+import uuid from 'uuid';
 
 class AgregarCita extends React.Component {
-  state = {};
+  state = {
+      error: false
+  };
 
-  render() {
-    return (
-      <div className="card mt-5">
-        <div className="card-body">
-            <h2 className="card-title text-center">Agregar Cita</h2>
-            <form>
-                <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Mascota: </label>
-                    <div className="col-sm-9">
-                        <input type="text" className="form-control" placeholder="Nombre Mascota"/>
+  nombreRef         = React.createRef();
+  propietarioRef    = React.createRef();
+  sintomasRef       = React.createRef();
+  fechaRef          = React.createRef();
+  horaRef           = React.createRef();
+
+    crearCita = (e) => {
+        e.preventDefault();
+
+        const nombre = this.nombreRef.current.value, 
+            propietario = this.propietarioRef.current.value,
+            sintomas = this.sintomasRef.current.value,
+            fecha = this.fechaRef.current.value,
+            hora = this.horaRef.current.value;
+
+        if( nombre === '' || propietario === '' || sintomas === '' || fecha === '' || hora === '' ){
+            this.setState({ error : true });
+        }else{
+            const cita = { id: uuid(), nombre, propietario, sintomas, fecha, hora }
+            this.props.crearCita(cita);
+            e.currentTarget.reset();
+            this.setState({ error : false })
+        }
+    }
+
+    
+    render() {
+
+        const existError = this.state.error;
+        
+        return (
+        <div className="card mt-5">
+            <div className="card-body">
+                <h2 className="card-title text-center">Agregar Cita</h2>
+                <form onSubmit={ this.crearCita }>
+                    <div className="form-group row">
+                        <label className="col-sm-3 col-form-label">Mascota: </label>
+                        <div className="col-sm-9">
+                            <input ref={ this.nombreRef } type="text" className="form-control" placeholder="Nombre Mascota"/>
+                        </div>
                     </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Propietario: </label>
-                    <div className="col-sm-9">
-                        <input type="text" className="form-control" placeholder="Nombre Propietario"/>
+                    <div className="form-group row">
+                        <label className="col-sm-3 col-form-label">Propietario: </label>
+                        <div className="col-sm-9">
+                            <input ref={ this.propietarioRef } type="text" className="form-control" placeholder="Nombre Propietario"/>
+                        </div>
                     </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Sintomas:</label>
-                    <div className="col-sm-9">
-                        <textarea className="form-control"></textarea>
+                    <div className="form-group row">
+                        <label className="col-sm-3 col-form-label">Sintomas:</label>
+                        <div className="col-sm-9">
+                            <textarea ref={ this.sintomasRef } className="form-control"></textarea>
+                        </div>
                     </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-md-2 col-form-label">Fecha:</label>
-                    <div className="col-md-4">
-                        <input type="date" className="form-control" />
+                    <div className="form-group row">
+                        <label className="col-md-2 col-form-label">Fecha:</label>
+                        <div className="col-md-4">
+                            <input ref={ this.fechaRef } type="date" className="form-control" />
+                        </div>
+
+                        <label className="col-md-2 col-form-label">Hora:</label>
+                        <div className="col-md-4">
+                            <input ref={ this.horaRef } type="time" className="form-control" min="08:00" max="18:00"/>
+                        </div>
                     </div>
 
-                    <label className="col-md-2 col-form-label">Hora:</label>
-                    <div className="col-md-4">
-                        <input type="time" className="form-control" />
+                    <div className="form-group row justify-content-center mt-3">
+                        <div className="col-sm-3">
+                            <button type="submit" className="btn btn-success w-100">Agregar</button>
+                        </div>
                     </div>
-                </div>
 
-                <div className="form-group row justify-content-center mt-3">
-                    <div className="col-sm-3">
-                        <button type="submit" className="btn btn-success w-100">Agregar</button>
-                    </div>
-                </div>
-            </form>
+                    { existError ? <div className="alert alert-danger text-center">Todos los campos son obligatorios.</div> : '' }
+                </form>
+            </div>
         </div>
-      </div>
     );
   }
 }
